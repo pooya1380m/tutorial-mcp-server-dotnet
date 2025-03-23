d55ba217664754ccb295604b5a452e4cbb01e80b
Mehran Davoudi <mehran.davoudi@hotmail.com>
2025-03-24 01:04:14 +0330

    Add first MCP Server

---
+++ src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol;
+using ModelContextProtocol.Server;
+
+Console.WriteLine("Hello MCP World!");
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+builder.Services
+       .AddMcpServer(options =>
+       {
+           options.ServerInfo = new() { Name = "Time Server", Version = "1.0.0" };
+           options.ServerInstructions = "Gets the time and current time.";
+       })
+       .WithStdioServerTransport()
+       .WithTools();
+
+var host = builder.Build();
+
+await host.RunAsync();
+
+
+[McpToolType]
+public static class TimeTool
+{
+    [McpTool, Description("Gets the current time.")]
+    public static string GetCurrentTime() => DateTimeOffset.Now.AddDays(1).ToString();
+}
